393470ae2ed3d407cf22ffa736b5981d07fbb50b
meticulousdev <meticulousdev@gmail.com>
2021-11-05 21:59:54 +0900

    file update

---
+++ chap02/julia_line_profiler01.py
@@ -0,0 +1,12 @@
+@profile
+def calculate_z_serial_purepython(maxiter, zs, cs):
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while abs(z) < 2 and n < maxiter:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
