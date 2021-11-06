f4662b347f4e0faf408f14ec12e9be317d2e2ffb
meticulousdev <meticulousdev@gmail.com>
2021-11-06 23:39:18 +0900

    no-op @profile

---
+++ chap02/utility.py
@@ -0,0 +1,19 @@
+import time
+
+if 'line_profiler' not in dir() and 'profile' not in dir():
+    def profile(func):
+        return func
+
+def test_some_fn():
+    assert some_fn(2) == 4
+    assert some_fn(1) == 1
+    assert some_fn(-1) == 1
+
+@profile
+def some_fn(useful_input):
+    time.sleep(1)
+    return useful_input ** 2
+
+
+if __name__ == "__main__":
+    print(f"Example call some_fn(2) == {some_fn(2)}")
