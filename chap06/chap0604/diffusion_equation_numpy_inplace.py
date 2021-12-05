bc4262f15dd769af2eef58e8e1ab6425c32ccf12
meticulousdev <meticulousdev@gmail.com>
2021-12-05 16:24:45 +0900

    roll add

---
+++ chap06/chap0604/diffusion_equation_numpy_inplace.py
@@ -0,0 +1,40 @@
+import time
+import numpy as np
+
+
+grid_shape = (640, 640)
+
+
+def laplacian(grid, out):
+    np.copyto(out, grid)
+    out *= -4
+    out += np.roll(grid, +1, 0)
+    out += np.roll(grid, -1, 0)
+    out += np.roll(grid, +1, 1)
+    out += np.roll(grid, -1, 1)
+
+
+@profile
+def evolve(grid, dt, out, D=1):
+    laplacian(grid, out)
+    out *= D * dt
+    out += grid
+
+
+def run_experiment(num_iterations):
+    next_grid = np.zeros(grid_shape)
+    grid = np.zeros(grid_shape)
+
+    block_low = int(grid_shape[0] * 0.4)
+    block_high = int(grid_shape[0] * 0.5)
+    grid[block_low:block_high, block_low:block_high] = 0.005
+
+    start = time.time()
+    for i in range(num_iterations):
+        evolve(grid, 0.1, next_grid)
+        grid, next_grid = next_grid, grid
+    return time.time() - start
+
+
+if __name__ == "__main__":
+    run_experiment(500)
---
+++ chap06/chap0604/diffusion_equation_roll_add.py
@@ -0,0 +1,64 @@
+import time
+import numpy as np
+
+
+grid_shape = (640, 640)
+
+
+def roll_add(rollee, shift, axis, out):
+    if shift == 1 and axis == 0:
+        out[1:, :] += rollee[:-1, :]
+        out[0, :] += rollee[-1, :]
+    elif shift == -1 and axis == 0:
+        out[:-1, :] += rollee[1:, :]
+        out[-1, :] += rollee[0, :]
+    elif shift == 1 and axis == 1:
+        out[:, 1:] += rollee[:, :-1]
+        out[:, 0] += rollee[:, -1]
+    elif shift == -1 and axis == 1:
+        out[:, :-1] += rollee[:, 1:]
+        out[:, -1] += rollee[:, 0]
+
+
+def test_roll_add():
+    rollee = np.asarray([[1, 2], [3, 4]])
+    for shift in (-1, +1):
+        for axis in (0, 1):
+            out = np.asarray([[6, 3], [9, 2]])
+            expected_result = np.roll(rollee, shift, axis=axis) + out
+            roll_add(rollee, shift, axis, out)
+            assert np.all(expected_result == out)
+
+
+def laplacian(grid, out):
+    np.copyto(out, grid)
+    out *= -4
+    roll_add(grid, +1, 0, out)
+    roll_add(grid, -1, 0, out)
+    roll_add(grid, +1, 1, out)
+    roll_add(grid, -1, 1, out)
+
+
+def evolve(grid, dt, out, D=1):
+    laplacian(grid, out)
+    out *= D * dt
+    out += grid
+
+
+def run_experiment(num_iterations):
+    next_grid = np.zeros(grid_shape)
+    grid = np.zeros(grid_shape)
+
+    block_low = int(grid_shape[0] * 0.4)
+    block_high = int(grid_shape[0] * 0.5)
+    grid[block_low:block_high, block_low:block_high] = 0.005
+
+    start = time.time()
+    for i in range(num_iterations):
+        evolve(grid, 0.1, next_grid)
+        grid, next_grid = next_grid, grid
+    return time.time() - start
+
+
+if __name__ == "__main__":
+    run_experiment(500)
