bc6eab428b72ea792009a9d6bc5f92542e443cd9
meticulousdev <meticulousdev@gmail.com>
2021-12-25 12:38:23 +0900

    update

--- chap07/ex0723_cffi/complicated_main.c
+++ chap07/ex0723_cffi/complicated_main.c
@@ -4,16 +4,14 @@
 int main(void)
 {
     struct Point pnt;
-    printf("complicated test\n");
-
     pnt.x = 0.0;
     pnt.y = 0.0;
     pnt.isActive = true;
     pnt.id = "center";
     pnt.num_times_visited = 0;
 
-    printf("point\n");
-    printf("(%f, %f)\n", pnt.x, pnt.y);
+    printf("point info.\n");
+    printf("position: (%f, %f)\n", pnt.x, pnt.y);
     printf("id: %s\n", pnt.id);
     printf("visited: %d\n", pnt.num_times_visited);
 
---
+++ chap07/ex0725_f2py/diffusion.f90
@@ -0,0 +1,7 @@
+SUBROUTINE evlove(grid, next_grid, D, dt, N, M)
+    !f2py threadsafe
+    !f2py intent(in) grid
+    !f2py intent(inplace) next_grid
+    !f2py intent(in) D
+    !f2py intent(in) dt
+    !f2py intent(hide) N
