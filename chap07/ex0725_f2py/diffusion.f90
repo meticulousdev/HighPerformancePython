d2638614cfda3743c36f17bd4ed595a588fdc9d9
meticulousdev <meticulousdev@gmail.com>
2021-12-25 12:43:03 +0900

    fortran code

--- chap07/ex0725_f2py/diffusion.f90
+++ chap07/ex0725_f2py/diffusion.f90
@@ -5,3 +5,13 @@ SUBROUTINE evlove(grid, next_grid, D, dt, N, M)
     !f2py intent(in) D
     !f2py intent(in) dt
     !f2py intent(hide) N
+    !f2py intent(hide) M
+    INTEGER :: N, M
+    DOUBLE PRECISION, DIMENSION(N, M) :: grid, next_grid
+    DOUBLE PRECISION, DIMENSION(N-2, M-2) :: laplacian
+    DOUBLE PRECISION :: D, dt
+
+    laplacian = grid(3:N, 2:M-1) + grid(1:N-2, 2:M-1) + &
+                grid(2:N-1, 3:M) + grid(2:N-1, 1:M-2) - 4 * grid(2:N-1, 2:M-1)
+    next_grid(2:N-1, 2:M-1) = grid(2:N-1, 2:M-1) + D * dt * laplacian
+END SUBROUTINE evolve
